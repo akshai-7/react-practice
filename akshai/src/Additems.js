c327d438c92f8d3b0141ea7b669cddbcec45bb66
Your Namekshai-7 <akshai2537@gmail.com>
2023-06-15 15:56:33 +0530

    first commit

--- akshai/src/Additems.js
+++ akshai/src/Additems.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { FaPlus } from 'react-icons/fa'
-const Additems = () => {
+const Additems = ({newItem,setNewItem,handleSubmit}) => {
   return (
-    <form action="" className='addForm'>
+    <form action="" className='addForm' onSubmit={handleSubmit}>
           <label htmlFor="addItem">Add Items</label>
-          <input type="text" autoFocus id='addItem' placeholder='Add item' required />
+          <input type="text" autoFocus id='addItem' placeholder='Add item' required  value={newItem} onChange={(e) => setNewItem(e.target.value)}/>
           <button type='submit' aria-label="Add Item">< FaPlus/></button>
     </form>
     
   )
 }
 
-export default Additems
+export default Additems 
--- akshai/src/App.js
+++ akshai/src/App.js
@@ -1,4 +1,3 @@
-// import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
 import Content from './Content';
@@ -7,28 +6,40 @@ import './index.css';
 import { useState } from 'react';
 import Additems from './Additems';
 function App() {
-    const [items, setItems] = useState([
-        {
-        id: 1,
-        checked: true,
-        item: "Practice coding"
-        },
-        {
-        id: 2,
-        checked: false,
-        item: "Play Cricket"
-        },
-        {
-        id: 3,
-        checked: false,
-        item: "Read About AI"
-        },
-         {
-        id: 4,
-        checked: false,
-        item: "Learn React"
-        }
-    ]) 
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem('to do list'))
+    // [
+    //     {
+    //     id: 1,
+    //     checked: true,
+    //     item: "Practice coding"
+    //     },
+    //     {
+    //     id: 2,
+    //     checked: false,
+    //     item: "Play Cricket"
+    //     },
+    //     {
+    //     id: 3,
+    //     checked: false,
+    //     item: "Read About AI"
+    //     },
+    //      {
+    //     id: 4,
+    //     checked: false,
+    //     item: "Learn React"
+    //     }
+    // ]
+  ) 
+    const [newItem, setNewItem] = useState('')
+    const addItem= (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const addNewItem = {id,checked:false, item }
+    const listItems = [...items, addNewItem]
+    setItems(listItems)
+    localStorage.setItem("to do list", JSON.stringify(listItems));
+    }
+  
     const handleCheck = (id) => {
         const listItems = items.map((item) =>
             item.id === id ? { ...item, checked: !item.checked } : item);
@@ -40,11 +51,21 @@ function App() {
         setItems(listItems);
          localStorage.setItem("to do list", JSON.stringify(listItems));
     }
- 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItem) return;
+    console.log('newItem');
+    addItem(newItem)
+    setNewItem('')
+    }
   return (
     <div>      
       <Header title="To Do List" />
-      <Additems/>
+      <Additems
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
       <Content
         items={items}
         handleCheck={handleCheck}
