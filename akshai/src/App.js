92f1c6b99609990231ed2d5525e440412dd721d5
Your Namekshai-7 <akshai2537@gmail.com>
2023-06-19 16:42:46 +0530

    first commit

--- akshai/src/App.js
+++ akshai/src/App.js
@@ -6,6 +6,7 @@ import './index.css';
 import { useEffect, useState } from 'react';
 import Additems from './Additems';
 import Searchitem from './Searchitem';
+import apiRequest from './apiRequest';
 function App() {
   const API_URL = 'http://localhost:3500/items';
   const [items, setItems] = useState([]);
@@ -33,24 +34,49 @@ function App() {
     },2000)
     },[])
   
-    const addItem= (item) => {
+  const addItem = async (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
-    const addNewItem = {id,checked:false, item }
+    const addNewItem = { id, checked: false, item }
     const listItems = [...items, addNewItem]
     setItems(listItems)
    
+    const postOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify(addNewItem)
+    }
+
+    const result = await apiRequest(API_URL, postOptions);
+    if (result) setFetchError(result);
+
     }
   
-    const handleCheck = (id) => {
+    const handleCheck = async (id) => {
         const listItems = items.map((item) =>
             item.id === id ? { ...item, checked: !item.checked } : item);
-        setItems(listItems);
-        
+      setItems(listItems);
+      
+      const myItem =listItems.filter(item=>item.id===id)
+        const updateOptions = {
+      method: 'PATCH',
+      headers: {
+        'Content-Type':'application/json'
+      },
+          body: JSON.stringify({ checked:myItem[0].checked })
     }
-    const handleDelete = (id) => {
+    const reqUrl =`${API_URL}/${id}`
+    const result = await apiRequest(reqUrl, updateOptions);
+    if (result) setFetchError(result);
+    }
+    const handleDelete =async (id) => {
         const listItems = items.filter((item) => item.id !== id);
         setItems(listItems);
-       
+      const deleteOptions = {method:'DELETE'}   
+    const reqUrl =`${API_URL}/${id}`
+    const result = await apiRequest(reqUrl, deleteOptions);
+    if (result) setFetchError(result);
     }
   const handleSubmit = (e) => {
     e.preventDefault();
